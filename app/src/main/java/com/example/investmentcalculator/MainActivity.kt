0bc5c61295628bf88bd0164b7bdfa458bbb401af
SaanaLiisaV <saanaliisavain@gmail.com>
2026-04-28 20:44:17 +0300

    Initial commit: Android Studio project for Investment Calculator

---
+++ app/src/main/java/com/example/investmentcalculator/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.investmentcalculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.investmentcalculator.ui.theme.InvestmentCalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            InvestmentCalculatorTheme {
+                enableEdgeToEdge()
+                Column(modifier = Modifier.padding(16.dp))
+                {ReactiveInvestmentValueUI()}
+            }
+        }
+    }
+}
+
+@Composable
+fun ReactiveInvestmentValueUI() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    InvestmentCalculatorTheme {
+        ReactiveInvestmentValueUI()
+    }
+}
